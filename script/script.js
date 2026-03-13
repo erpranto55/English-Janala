35609c8b893df6cc2887e3840b04b50f851f1ef3
erpranto <erpranto55@gmail.com>
2026-03-13 22:31:07 +0600

    Word Container Section Edited

--- script/script.js
+++ script/script.js
@@ -4,25 +4,48 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
+const removeActive = () => {
+  const lessonBtn = document.querySelectorAll(".lesson-btn");
+  lessonBtn.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
+
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWords(data.data));
+    .then((data) => {
+      removeActive();
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active");
+      displayLevelWords(data.data);
+    });
 };
 
 const displayLevelWords = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+        <div class="text-center col-span-full space-y-5 font-bangla">
+            <img src="./assets/alert-error.png" alt="" class=mx-auto>
+            <p class="text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-[#292524] font-medium text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+    `;
+    return;
+  }
   words.forEach((word) => {
     const card = document.createElement("div");
     card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-8">
           <div class="space-y-5">
-            <h2 class="font-bold text-3xl">${word.word}</h2>
+            <h2 class="font-bold text-3xl">${word.word ? word.word : "No Word Found"}</h2>
             <p class="font-semibold text-xl">Meaning /Pronunciation</p>
             <p class="font-bangla font-semibold opacity-[80%] text-3xl">
-              "${word.meaning} / ${word.pronunciation}"
+              "${word.meaning ? word.meaning : "No Meaning Found"} / ${word.pronunciation ? word.pronunciation : "No Pronunciation Found"}"
             </p>
             <div class="flex justify-between items-center">
               <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
@@ -48,7 +71,7 @@ const displayLessons = (lessons) => {
     // 3.create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary" onclick="loadLevelWord(${lesson.level_no})">
+    <button id="lesson-btn-${lesson.level_no}" class="btn btn-outline btn-primary lesson-btn" onclick="loadLevelWord(${lesson.level_no})">
     <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
     </button>
     `;
